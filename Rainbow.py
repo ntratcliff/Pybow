de80de556e0d8a0bcc68f8cfb2a4bd638aa66dd7
Noah Ratcliff <me@nratcliff.com>
2016-03-01 19:04:52 -0500

    Basic statement lexing

--- Rainbow.py
+++ Rainbow.py
@@ -4,6 +4,8 @@ from PIL import Image
 def rgb2hex(r, g, b):
 	return '{:02x}{:02x}{:02x}'.format(r,g,b)
 
+def 
+
 if len(sys.argv) > 1:
 	imgPath = sys.argv[1]
 else: 
@@ -11,6 +13,15 @@ else:
 
 img = Image.open(imgPath)
 
+statements = []
 
 for r, g, b in list(img.getdata()):
-	print rgb2hex(r, g, b)
+	statements.append(rgb2hex(r, g, b))
+
+mem = bytearray(256)
+
+for statement in statements:
+	ins = statement[0]
+	addr = statement[1:2]
+	switch = statement[3]
+	val = statement[4:5]	
